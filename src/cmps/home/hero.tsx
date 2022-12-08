a132439cc0260855a34096442696d93b04a7174b
Eytan Silberberg <eytan@Eytans-MacBook-Pro.local>
2022-12-08 18:53:09 +0200

    worked on hero imgs carousel

--- src/cmps/home/hero.tsx
+++ src/cmps/home/hero.tsx
@@ -1,6 +1,34 @@
+import { useEffect, useRef, useState } from "react"
+
 export const Hero = () => {
+    const imgLinks: string[] = ['https://media.resources.festicket.com/image/fit-in/1580x1046/filters:quality(70)/www/homepage-headers/SilencioUmbra_Hero.jpeg', 'https://media.resources.festicket.com/image/fit-in/1580x1046/filters:quality(70)/www/homepage-headers/SouthportWeekender2023.jpeg', 'https://media.resources.festicket.com/image/fit-in/1580x1046/filters:quality(70)/www/homepage-headers/MadCool2023.jpeg']
+
+    const currImgNumRef = useRef(0)
+    const [currImg, setImg] = useState<string>(imgLinks[0])
+    const [isCoverOn, setCover] = useState<boolean>(false)
+
+    useEffect(() => {
+        // setCover(true)
+        setInterval(() => {
+            let currNum = currImgNumRef.current
+            console.log(currNum);
+            if (currNum + 1 === 3) {
+                currImgNumRef.current = 0
+                currNum = 0
+            }
+            else {
+                currImgNumRef.current = currImgNumRef.current + 1
+                currNum += 1
+            }
+            setImg(imgLinks[currNum])
+
+        }, 1500)
+
+    }, [])
+
+
     return <section className='hero'>
         <h3>Book it. Love it. <span>Enjoy it</span></h3>
-        <p>imgs</p>
+        <img src={currImg} alt="" />
     </section>
 }
