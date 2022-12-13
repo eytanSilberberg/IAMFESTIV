6a46dfab0cc5942197d0da2dc03bd725c04c2c2e
reemnu <reemnu@gmail.com>
2022-12-13 11:48:34 +0200

    login page update

--- src/pages/log_in.tsx
+++ src/pages/log_in.tsx
@@ -1,14 +1,40 @@
-export const Log_In = () => {
+import React, {useState} from "react";
+
+export const Log_In: React.FC = () => {
+  
+  const [username, setUsername] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  const handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void = (e) => {
+    e.preventDefault()
+    const loginData = new URLSearchParams();
+    loginData.append('username', username)
+    loginData.append('password', password)
+    fetch('http://127.0.0.1:5000/login', {
+      method: 'post',
+      body: loginData,
+    })
+    .then(res => {
+      if (res.status === 200) {
+        console.log('logged in!');
+      }
+      else {
+        console.log('NOT logged in!');
+      }
+      //res.status === 200 ? undefined : undefined
+    }).catch(err => console.error(err))
+    }
+
   return (
     <section className="log in main-layout">
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           username:
-          <input type="text" />
+          <input type="text" value={username} onChange={(e): void => {setUsername(_ => e.target.value)}}/>
         </label>
         password:
-        <input type="text" />
-        <input type="submit" value="Submit" />
+        <input type="password" value={password} onChange={(e): void => {setPassword(_ => e.target.value)}}/>
+        <button type="submit"/>
       </form>
     </section>
   );
